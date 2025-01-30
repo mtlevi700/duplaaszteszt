2debc5707ca8f994d1b35b8b13bc0e95ee648acd
mtlevi700 <hegyi.levente24@gmail.com>
2025-01-30 19:09:27 +0100

    innit

---
+++ navbar_mobile.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let bars = document.querySelector('.bars');
+    let close = document.querySelector('.close');
+    let nav = document.querySelector('nav');
+
+    bars.addEventListener('click', function () {
+        bars.style.display = 'none';
+        close.style.display = 'flex';
+        nav.style.display = 'block';
+        setTimeout(() => {
+            nav.classList.add('active');
+        }, 10); // Kis késleltetés az animáció elindításához
+    });
+
+    close.addEventListener('click', function () {
+        bars.style.display = 'flex';
+        close.style.display = 'none';
+        nav.classList.remove('active');
+        setTimeout(() => {
+            if (!nav.classList.contains('active')) {
+                nav.style.display = 'none';
+            }
+        }, 500); // Várakozás az animáció befejezéséig
+    });
+});
